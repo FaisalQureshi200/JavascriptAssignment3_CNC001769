33a8603560f80a5f2753775d496bf8ef6fc2fe11
faisalqureshi <faisalqureshi690@gmail.com>
2019-12-16 11:08:22 +0500

    This is Task6 file

---
+++ Task6/Task6.js
@@ -0,0 +1,6 @@
+var cities = ["Karachi", "Lahore", "Islamabad", "Faisalabad"];
+var o = ["th","st","nd","rd"];
+document.write('1'+o[1]+'  choice is '+cities[0]+'<br>')
+document.write('2'+o[2]+'  choice is '+cities[1]+'<br>')
+document.write('3'+o[3]+'  choice is '+cities[2]+'<br>')
+document.write('4'+o[0]+'  choice is '+cities[3]+'<br>')
