e668910aa7144af2735a8a6921bee16352ca06ae
faisalqureshi <faisalqureshi690@gmail.com>
2019-12-16 11:07:40 +0500

    This is Task2 file

---
+++ Task2/Task2.js
@@ -0,0 +1,13 @@
+var names=["Omer", "Moiz", "Zain"]
+var marks=[400,350,450]
+
+
+    var percentage=marks[0]*100/500
+    document.write('<h3>'+'Score of '+names[0]+ ' is ' +marks[0]+'. Percentage: '+percentage+'%'+'</h3>')
+    var percentage=marks[1]*100/500
+    document.write('<h3>'+'Score of '+names[1]+ ' is ' +marks[1]+'. Percentage:'+percentage+'%'+'</h3>')
+    var percentage=marks[2]*100/500
+    document.write('<h3>'+'Score of '+names[2]+ ' is ' +marks[2]+'. Percentage: '+percentage+'%'+'</h3>')
+
+  
+
