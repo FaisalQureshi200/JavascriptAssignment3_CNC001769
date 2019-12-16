bdbb90bbc0eda7489208c4b11c7b9c4ba926341e
faisalqureshi <faisalqureshi690@gmail.com>
2019-12-16 11:08:00 +0500

    This is Task4 file

---
+++ Task4/Task4.js
@@ -0,0 +1,5 @@
+var cityname=['Karachi','Lahore','Islamabad','Quetta','Peshawar']
+document.write('<h2>'+'Cities List'+'</h2>')
+document.write(cityname)
+document.write('<h2>'+'Selected Cities List'+'</h2>')
+document.write(cityname.slice(1,3))
